c96c802c356ee70ffa6e399e232ae5cd206b117c
xevansz <minithbmatthew@gmail.com>
2026-02-11 11:43:07 +0530

    validate email&&password and show errors

---
+++ frontend/src/auth/validate.jsx
@@ -0,0 +1,32 @@
+export const validateEmail = (email) => {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+};
+
+export const validateLoginForm = (email, password) => {
+  const errors = {};
+
+  if (!email) {
+    errors.email = "Email is required";
+  } else if (!validateEmail(email)) {
+    errors.email = "Please enter a valid email address";
+  }
+
+  if (!password) {
+    errors.password = "Password is required";
+  } else if (password.length < 6) {
+    errors.password = "Password must be at least 6 characters";
+  }
+
+  return errors;
+};
+
+export const validateRegisterForm = (email, password, confirmPassword) => {
+  const errors = validateLoginForm(email, password, confirmPassword);
+
+  if (password !== confirmPassword) {
+    errors.confirmPassword = 'Passwords do not match';
+  }
+
+  return errors;
+};

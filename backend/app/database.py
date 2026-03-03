53ff344ceffaa8678d6c9d60e0b0bd8e1a70a757
xevansz <minithbmatthew@gmail.com>
2026-03-04 00:36:26 +0530

    fix config infra

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,6 +1,7 @@
 """Database Initialization"""
 
 from motor.motor_asyncio import AsyncIOMotorClient
+from pymongo.errors import PyMongoError
 
 from app.config import settings
 
@@ -11,9 +12,17 @@ class Database:
         self.db = None
 
     async def connect(self):
-        self.client = AsyncIOMotorClient(settings.MONGODB_URI)
-        self.db = self.client[settings.DB_NAME]
-        print("MongoDB connected")  # TODO: check to see if it really connected to mongodb
+        try:
+            self.client = AsyncIOMotorClient(settings.MONGODB_URI, serverSelectionTimeoutMS=5000)
+            self.db = self.client[settings.DB_NAME]
+
+            await self.client.admin.command("ping")
+
+            print("Mongodb connected succesfully")
+
+        except PyMongoError as e:
+            print(f"MongoDB connected failed: {e}")
+            raise
 
     async def close(self):
         if self.client:
@@ -26,11 +35,21 @@ class Database:
     async def create_index_symbol(self):  # Do we need this?
         if self.db is not None:
             collection = self.db["symbol_cache"]
-            await collection.create_index([("company_name", 1)])
+            await collection.create_index([("symbol", 1)])
 
     async def create_index_watchlist(self):
         if self.db is not None:
             await self.db.watchlist.create_index([("user_id", 1), ("symbol", 1)], unique=True)
+            # obj = await self.db.watchlist.find({"user_id": user_id}).explain("executionStats")
+            # if obg is COLLSCAN we need an index
+
+    async def create_index_users(self):
+        if self.db is not None:
+            await self.db.users.create_index("email", unique=True)
+
+    async def create_index_conversations(self):
+        if self.db is not None:
+            await self.db.conversations.create_index([("user_id", 1), ("created_at", -1)])
 
 
 database = Database()
@@ -47,3 +66,5 @@ async def close_databases():
 async def create_index_cache():
     await database.create_index_symbol()
     await database.create_index_watchlist()
+    await database.create_index_users()
+    await database.create_index_conversations()
